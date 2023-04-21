9f21660dfd70f4919667ed64290f7d94af62a2d3
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-04-21 23:32:17 +0700

    разобрал основные моменты

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+export default {
+  data() {
+    return {
+      text: 'page',
+      href: 'page.html',
+
+      num1: 4,
+      num2: 5,
+
+      myArray: [1, 2, 3],
+
+      myObject: {a: 111, b: 222, c: 333},
+
+
+    }
+  },
+
+  // кастомные методы
+  methods: {
+    myMethod: function () {
+      alert(new Date());
+    },
+
+    myMethodHi: function () {
+      alert(this.num1 + this.num2);
+    },
+
+    x2method: function (num) {
+      return num * 2;
+    },
+
+    // вызов метода через this
+    show: function () {
+      alert(this.x2method(10));
+    },
+  }
+}
+
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- формирование ссылки -->
+  <a v-bind:href="href">{{ text }}</a>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- выполнение js кода в фигурных скобках -->
+  <p>суммирование свойств из 'data': {{ num1 + num2 }}</p>
+
+
+  <!-- вывод из массива по индексу -->
+  <p> вывод из массива:
+    {{ myArray[0] }},
+    {{ myArray[2] }}
+  </p>
+
+  <!-- вывод из объекта -->
+  <p>вывод из объекта: {{ myObject.a }}</p>
+  <p>сумма свойств объекта: {{ myObject.a + myObject.b }}</p>
+
+  <!-- вызов метода -->
+  <!-- {{ myMethod() }} -->
+
+  <!-- вызов обработчика по клику-->
+  <button v-on:click="myMethod">кнопка</button>
+  <button @:click="myMethodHi">кнопка#2</button>
+  <button @:click="show">кнопка#3</button>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
